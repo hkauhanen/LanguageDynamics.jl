c4ae4356b4cdc50b3ef8db2c25c4fd5005e79fd7
Documenter.jl <documenter@juliadocs.github.io>
2021-06-09 07:26:50 +0000

    build based on adc146f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LanguageDynamics","category":"page"},{"location":"#LanguageDynamics","page":"Home","title":"LanguageDynamics","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LanguageDynamics.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LanguageDynamics]","category":"page"}]
+}
